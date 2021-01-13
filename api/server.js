1bcf8fa9555e2da57b3c05e627963b74b922d5dc
Juan Aleman <juan.j.aleman@gmail.com>
2021-01-13 00:19:14 -0600

    feature: added jsonwebtoken and bcryptjs dependencies

--- api/server.js
+++ api/server.js
@@ -5,7 +5,10 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const server = express();
+
 server.use(helmet());
+server.use(express.json());
+server.use(cors());
 
 server.get("/", (req, res) => {
     res.status(200).json({ message: "Server is up!",  testObject: process.env.TEST});
